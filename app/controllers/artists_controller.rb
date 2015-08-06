94658e8d292c998b76d9c21d28c7be7068bbdaf2
Samuel Key <s.key08@gmail.com>
2015-08-06 15:21:33 -0400

    attempting to get css images to work on heroku

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,10 +2,6 @@ class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
 
-  def allartists
-    @artists = Artist.all
-  end
-
   #index
   def index
     if current_user
