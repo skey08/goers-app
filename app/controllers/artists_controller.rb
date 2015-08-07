dd35c6eecd9ebd864a7a6da38749ef80dccb8342
Christine Lovett <clovett07@gmail.com>
2015-08-07 16:10:35 -0400

    pair refactor

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,50 +2,35 @@ class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
 
-  #index
   def index
-    # if current_user
-    #   @artists = current_user.artists.order('name ASC')
-    # else
-      @artists = Artist.all.order('name ASC')
-    # end
+    @artists = Artist.all.order('name ASC')
   end
 
   def myartists
     @artists = current_user.artists.order('name ASC')
   end
 
-
-  #new
   def new
     @artist = Artist.new
   end
 
-  #create
   def create
-    # @artist = Artist.create!(artist_params)
     @artist = current_user.artists.create!(artist_params)
     redirect_to artist_path(@artist)
   end
 
-  #show
   def show
   end
 
-  #edit
   def edit
   end
 
-  #update
   def update
-
     @artist.update(artist_params)
     redirect_to artist_path(@artist)
   end
 
-  #destroy
   def destroy
-
     @artist.destroy
     redirect_to artists_path
   end
